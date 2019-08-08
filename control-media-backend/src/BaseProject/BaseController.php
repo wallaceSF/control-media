fadaf71b87aeadfd7ba991070a0e53dd20bd5372
wallacesf87 <wallace.sf87@gmail.com>
2019-08-07 22:43:08 -0300

    add try/catch

--- control-media-backend/src/BaseProject/BaseController.php
+++ control-media-backend/src/BaseProject/BaseController.php
@@ -10,7 +10,12 @@ abstract class BaseController
 {
     function __invoke(Request $request, Response $response, array $args): ResponseInterface
     {
-        return $this->handle($request, $response, $args);
+        try {
+            return $this->handle($request, $response, $args);
+        } catch (\Exception $e) {
+            $statusCode = ($e->getCode() > 100) ? $e->getCode() : 500;
+            return $response->withJSON([$e->getMessage()], $statusCode,JSON_PRETTY_PRINT);
+        }
     }
 
     abstract public function handle(Request $request, Response $response, array $args): ResponseInterface;
