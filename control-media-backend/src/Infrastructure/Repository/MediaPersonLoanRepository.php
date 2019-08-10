06abfa4baf2d5b4d25105a748a6410a622ee6039
Wallace Ferreira <wallace.sf87@gmail.com>
2019-08-10 18:47:58 -0500

    fix query getLastestMediaData

--- control-media-backend/src/Infrastructure/Repository/MediaPersonLoanRepository.php
+++ control-media-backend/src/Infrastructure/Repository/MediaPersonLoanRepository.php
@@ -5,33 +5,72 @@ namespace App\Infrastructure\Repository;
 use App\Domain\Contract\Domain\DataBaseBuildInterface;
 use App\Domain\Contract\Infrastruture\Repository\MediaPersonLoanRepositoryInterface;
 use App\Domain\Entity\MediaPersonLoan;
+use App\Domain\ValueObject\MediaPersonLoanVO;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 
 class MediaPersonLoanRepository implements MediaPersonLoanRepositoryInterface
 {
     /**
      * @var DataBaseBuildInterface
      */
-    private $dataBaseBuildInterface;
+    private $dataBaseBuild;
 
     /**
      * MediaRepository constructor.
-     * @param DataBaseBuildInterface $dataBaseBuildInterface
+     * @param DataBaseBuildInterface $dataBaseBuild
      */
-    public function __construct(DataBaseBuildInterface $dataBaseBuildInterface)
+    public function __construct(DataBaseBuildInterface $dataBaseBuild)
     {
-        $this->dataBaseBuildInterface = $dataBaseBuildInterface;
+        $this->dataBaseBuild = $dataBaseBuild;
     }
 
     public function findAll(): array {
-        return $this->dataBaseBuildInterface->getRepository(MediaPersonLoan::class)->findAll();
+        return $this->dataBaseBuild->getRepository(MediaPersonLoan::class)->findAll();
     }
 
     public function getLastestMediaData($mediaId): ?MediaPersonLoan
     {
+        $sql = "SELECT * FROM media_person_loan where media = ? ORDER BY CASE WHEN returned_at IS NULL THEN 1 ELSE 0 END DESC, id desc, returned_at DESC limit 100 ";
+         
+        $conn = $this->dataBaseBuild->getEntityManager()
+            ->getConnection();       
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $mediaId);
+        $stmt->execute();
+        $id = $stmt->fetchAll();
+
         /** @var MediaPersonLoan|null $mediaPersonLoan */
-        $mediaPersonLoan = $this->dataBaseBuildInterface->getRepository(MediaPersonLoan::class)->findOneBy(
-            ['media'=> $mediaId],
+        $mediaPersonLoan = $this->dataBaseBuild->getRepository(MediaPersonLoan::class)->findOneBy(
+            ['media'=> $mediaId, 'id' => $id[0]['id']],
             ['returnedAt' => 'DESC']
+        );
+
+        return $mediaPersonLoan;
+    }
+
+    public function persist(MediaPersonLoan $media): MediaPersonLoan
+    {
+        $this->dataBaseBuild->persist($media);
+        $this->dataBaseBuild->flush();
+
+        return $media;
+    }
+
+    public function findByPersonAndMedia(MediaPersonLoanVO $mediaPersonLoanVO): ?MediaPersonLoan
+    {
+        $sql = "SELECT * FROM media_person_loan where media = ? ORDER BY CASE WHEN returned_at IS NULL THEN 1 ELSE 0 END DESC, id desc, returned_at DESC limit 100 ";
+         
+        $conn = $this->dataBaseBuild->getEntityManager()
+            ->getConnection();       
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $mediaPersonLoanVO->media);
+        $stmt->execute();
+        $id = $stmt->fetchAll();
+
+        /** @var MediaPersonLoan|null $mediaPersonLoan */
+        $mediaPersonLoan = $this->dataBaseBuild->getRepository(MediaPersonLoan::class)->findOneBy(
+            ['media'=> $mediaPersonLoanVO->media, 'id' => $id[0]['id']],
+            ['id' => 'DESC']
         );
 
         return $mediaPersonLoan;
