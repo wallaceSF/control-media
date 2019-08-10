eaa594ff7e1d54a9ffaa79ea617b2f5d7c47e10f
Wallace Ferreira <wallace.sf87@gmail.com>
2019-08-10 18:56:59 -0500

    remove restriction to make request for everyone

--- control-media-backend/public/index.php
+++ control-media-backend/public/index.php
@@ -1,4 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
